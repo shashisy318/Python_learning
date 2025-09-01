09964739e458c0ffc509bbb307dd5469db99c73c
shashisy318 <shekharshashi318@gmail.com>
2025-09-01 22:26:52 +0530

    Added chapter 8

---
+++ Chapter_8/01_function_basics.py
@@ -0,0 +1,18 @@
+# Functions :-> 
+'''
+-> A function is a block of code performing some specific task 
+whenever called using its name.
+-> when a particular set of code is repeated at different places in 
+the code, its better to improve efficiency by giving that particular 
+set of code a name through which we can call to perform that code 
+from there the concept of function arises. 
+-> it can be called any number of time in a program.
+'''
+
+# Syntax ->
+'''
+
+def FUNCTION_NAME(PARAMETER):
+    #STATEMENT
+    
+'''
